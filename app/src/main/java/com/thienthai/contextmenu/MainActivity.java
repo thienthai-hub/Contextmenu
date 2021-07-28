50f600369ea9206a770c7a3da92b921ae7a7f36e
thienthai-hub <vohuyenthienthai94@gmail.com>
2021-07-28 11:01:07 +0700

    change backgroud color Context menu you must click long to show menu

--- app/src/main/java/com/thienthai/contextmenu/MainActivity.java
+++ app/src/main/java/com/thienthai/contextmenu/MainActivity.java
@@ -19,37 +19,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btnColor = (Button) findViewById(R.id.btnColor);
-        constraintLayout = (ConstraintLayout) findViewById(R.id.layout);
+            btnColor = (Button) findViewById(R.id.btnColor);
+            constraintLayout = (ConstraintLayout) findViewById(R.id.layout);
 
-        //đăng kí view cho context menu
-        registerForContextMenu(btnColor);
+            //đăng kí view cho context menu
+            registerForContextMenu(btnColor);
     }
 
     @Override
-    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
-        getMenuInflater().inflate(R.menu.menu, menu);
-        menu.setHeaderTitle("Chọn màu");
-        menu.setHeaderIcon(R.drawable.nv1);
+        public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+            getMenuInflater().inflate(R.menu.menu, menu);
+            menu.setHeaderTitle("Chọn màu");
+            menu.setHeaderIcon(R.drawable.nv1);
 
-        super.onCreateContextMenu(menu, v, menuInfo);
-    }
-
-    @Override
-    public boolean onContextItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId()){
-            case R.id.tim: constraintLayout.setBackgroundColor(Color.rgb(93,47,159));
-                break;
-            case R.id.vang: constraintLayout.setBackgroundColor(Color.rgb(255,235,59));
-                break;
-            case R.id.xanh: constraintLayout.setBackgroundColor(Color.rgb(33,150,243));
-                break;
-            case R.id.đo: constraintLayout.setBackgroundColor(Color.rgb(221,53,23));
-                break;
-
-            case R.id.đen: constraintLayout.setBackgroundColor(Color.rgb(0,0,0));
-                break;
+            super.onCreateContextMenu(menu, v, menuInfo);
         }
-        return super.onContextItemSelected(item);
-    }
+
+        @Override
+        public boolean onContextItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()){
+                case R.id.tim: constraintLayout.setBackgroundColor(Color.parseColor("#000000"));
+                    break;
+                case R.id.vang: constraintLayout.setBackgroundColor(Color.rgb(255,235,59));
+                    break;
+                case R.id.xanh: constraintLayout.setBackgroundColor(Color.rgb(33,150,243));
+                    break;
+                case R.id.đo: constraintLayout.setBackgroundColor(Color.rgb(221,53,23));
+                    break;
+
+                case R.id.đen: constraintLayout.setBackgroundColor(Color.rgb(0,0,0));
+                    break;
+            }
+            return super.onContextItemSelected(item);
+        }
 }
